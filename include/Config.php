d1c9a97f88bbd617a52c346dfd1e5f92284bd5c5
Annie <a>
2014-06-10 01:46:09 +0200

    files in the gitignore, just for have a tpl

---
+++ include/Config.php
@@ -0,0 +1,7 @@
+<?php
+$databaseHost = 'localhost';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+$databaseName = 'social';
+$path2Phps = '/Application/MAMP/htdocs/social/';
+$baseurl = 'http://application.loc/social/';?>
